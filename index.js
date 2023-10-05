7ee86f118de61607e09d54b3d70e16d4b2e2bd8d
Walter N <2760walter@gmail.com>
2023-10-05 14:47:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+
+function submitData(username, useremail){
+    const formData = {name:username,email:useremail,};
+    
+
+    return fetch('http://localhost:3000/users',{
+        method:"POST",
+        headers:{"Content-Type":"application/json",Accept:"application/json",},
+        body: JSON.stringify(formData),
+    }).then(res => res.json())
+    .then(obj => {
+        const userIdlbl = document.createElement('label');
+        userIdlbl.textContent =`user id: ${obj.id}`;
+        document.body.appendChild(userIdlbl);
+        console.log(obj);
+    }) .catch(function(error){
+        const userIdlbl = document.createElement('label');
+        userIdlbl.textContent =`oops!! : ${error.message}`;
+        document.body.appendChild(userIdlbl);
+        
+
+    })
+}
+
+// submitData("matphilips","philips@mat.com");
+// submitData("kevin","kevlips@mat.com");
+// submitData("dan","philips@dan.com");
+// submitData("hue","hue@mat.com");
+// submitData("mat","philips@mat.com");
+
+function DisplayUser(){
+    return fetch('http://localhost:3000/users')
+    .then(res =>res.json())
+    .then(users =>{
+        for(const user of users){
+            console.log(user.username)
+        }
+    })
+    .catch(function(error){
+        const userIdlbl = document.createElement('label');
+        userIdlbl.textContent =`oops!! : ${error.message}`;
+        document.body.appendChild(userIdlbl);
+        
+
+    })
+}
+//DisplayUser();
+submitData("jax","philips@mat.com");
+
+
+  
